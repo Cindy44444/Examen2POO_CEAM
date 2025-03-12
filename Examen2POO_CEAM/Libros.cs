fb39ba7b8c0dd80860e2c8fc71a84ef3077a3ada
Lab2-06 <Lab2-06@LAB2-06>
2025-03-11 18:17:54 -0600

    cambios

--- Examen2POO_CEAM/Libros.cs
+++ Examen2POO_CEAM/Libros.cs
@@ -8,7 +8,25 @@ namespace Examen2POO_CEAM
 {
     internal class Libros
     {
+        public Libros() { }
 
+        public Libros(string nombre, string editorial, string autor, int paginas, string genero)
+        {
+            this.nombre = nombre;
+            this.editorial = editorial;
+            this.autor = autor;
+            this.paginas = paginas;
+            this.genero = genero;
+        }
+
+
+
+        //atributos
+        public string nombre { get; set; }
+        public string editorial {  get; set; }
+        public string autor { get; set; }
+        public int paginas { get; set; }
+        public string genero { get; set; }
 
     }
 }
--- Examen2POO_CEAM/acciones.cs
+++ Examen2POO_CEAM/acciones.cs
@@ -8,6 +8,70 @@ namespace Examen2POO_CEAM
 {
     internal class acciones
     {
+        public List<Libros> listalibros = new List<Libros>();
+        Libros l = new Libros();
+
+        public void AgregarLibro()
+        {
+            Console.WriteLine("Nombre:");
+            l.nombre = Console.ReadLine();
+            Console.WriteLine("Editorial:");
+            l.editorial = Console.ReadLine();
+            Console.WriteLine("Autor:");
+            l.autor = Console.ReadLine();
+            Console.WriteLine("Paginas:");
+            l.paginas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Genero:");
+            l.genero = Console.ReadLine();
+
+            listalibros.Add(new Libros (l.nombre,l.editorial,l.autor,l.paginas,l.genero));
+        }
+        public void MostrarLibros()
+        {
+            foreach(var libro in listalibros)
+            {
+                Console.WriteLine($"Nombre:{libro.nombre}");
+                Console.WriteLine($"Editorial:{libro.editorial}");
+                Console.WriteLine($"Autor:{libro.autor}");
+                Console.WriteLine($"Paginas:{libro.paginas}");
+                Console.WriteLine($"Genero:{libro.genero}");
+
+            }
+        }
+        public int ContarLibros()
+        {
+            return listalibros.Count;
+        }
+        public void EliminarLibros(string nombrelibro)
+        {
+            var libro = listalibros.Find(x => x.nombre == nombrelibro);
+            if (libro != null)
+            {
+                listalibros.Remove(libro);
+            }
+            else
+            {
+                Console.WriteLine("No se encontro el libro");
+            }
+        }
+        public void Actualizar(string nombrelibro)
+        {
+            var libros = listalibros.Find(x => x.nombre == nombrelibro);
+            if (libros != null)
+            {
+                libros.nombre = Console.ReadLine();
+                libros.editorial = Console.ReadLine();
+                libros.autor = Console.ReadLine();
+                libros.paginas = Convert.ToInt32(Console.ReadLine());
+                libros.genero = Console.ReadLine();
+
+                Console.WriteLine("Codigo exitoso");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró ningún libro con ese nombre");
+            }
+        }
 
     }
 }
